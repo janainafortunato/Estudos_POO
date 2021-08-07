df7b3e5fb559d6c49b057463c068a3c52abed266
janainafortunato <janaina.projetos@live.com>
2021-08-07 17:34:23 -0300

    criação de class e metodos em POO

---
+++ dotnet-POO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using dotnet_POO.src.Entities;
+
+namespace dotnet_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero arus = new Hero("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+
+            Console.Write(arus.Attack());
+             Console.Write(wizard.Attack());
+           
+        }
+    }
+}
